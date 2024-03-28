e9243a5193fe703a6b95fc12a95a0f06f4a97ec3
ratherburhan <rathermuhiz@gmail.com>
2024-03-28 21:40:04 +0530

    Use env variables and use Procfile

--- main.py
+++ main.py
@@ -1,65 +1,37 @@
-from datetime import date
-from flask import Flask, abort, render_template, redirect, url_for, flash
+import datetime as dt
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
-from flask_gravatar import Gravatar
-from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Text, ForeignKey
-from functools import wraps
-from werkzeug.security import generate_password_hash, check_password_hash
-from typing import List
-from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
 import os
+import smtplib
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, EmailField
+from wtforms.validators import DataRequired, URL, Email
+from flask_ckeditor import CKEditorField
 
 
-def admin_only(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if not current_user.is_authenticated or current_user.id != 1:
-            return abort(403)
-        return f(*args, **kwargs)
+# WTForm for creating a Message
+class ContactForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email_address = StringField("Email Address", validators=[DataRequired("This field cannot be empty"), Email(
+        message='Invalid Email address')])
+    phone = StringField("Phone", validators=[DataRequired("This field cannot be empty")])
+    message = CKEditorField("Message", validators=[DataRequired("This field cannot be empty")])
+    submit = SubmitField("Submit Message")
 
-    return decorated_function
 
-
-def only_commenter(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        comment = db.get_or_404(Comment, kwargs["comment_id"])
-        # comment = db.session.execute(db.select(Comment).where(Comment.author_id == current_user.id)).scalar()
-        if not current_user.is_authenticated or current_user.id != comment.author_id:
-            return abort(403)
-        return f(*args, **kwargs)
-
-    return decorated_function
-
+my_email = os.environ.get('my_email')
+pwd = os.environ.get('pwd')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
-gravatar = Gravatar(app,
-                    size=100,
-                    rating='g',
-                    default='retro',
-                    force_default=False,
-                    force_lower=False,
-                    use_ssl=False,
-                    base_url=None)
-
-# TODO: Configure Flask-Login
-login_manager = LoginManager()
-login_manager.init_app(app)
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return db.get_or_404(User, user_id)
-
-
-# CREATE DATABASE
 class Base(DeclarativeBase):
     pass
 
@@ -69,204 +41,96 @@ db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
 
-# TODO: Create a User table for all your registered users.
-
-class User(UserMixin, db.Model):
-    __tablename__ = "users"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    email: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(String(250), nullable=False)
-    name: Mapped[str] = mapped_column(String(250), nullable=False)
-
-    # This will act like a List of BlogPost objects attached to each User.
-    # The "author" refers to the author property in the BlogPost class.
-    posts = relationship("BlogPost", back_populates="author")
-    comments = relationship("Comment", back_populates="author")
-
-    # posts: Mapped[List["BlogPost"]] = relationship(back_populates="author")
-
-
 # CONFIGURE TABLES
-class BlogPost(db.Model):
-    __tablename__ = "blog_posts"
+class Tour(db.Model):
+    __tablename__ = "tours"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    # Create Foreign Key, "users.id" the users refers to the tablename of User.
-    author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
-    # author_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    # Create reference to the User object. The "posts" refers to the posts property in the User class.
-    author = relationship("User", back_populates="posts")
-    # author: Mapped["User"] = relationship(back_populates="posts")
-    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
-    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
-    date: Mapped[str] = mapped_column(String(250), nullable=False)
-    body: Mapped[str] = mapped_column(Text, nullable=False)
+    title: Mapped[str] = mapped_column(String(250), nullable=False)
+    location: Mapped[str] = mapped_column(String(250), nullable=False)
+    time: Mapped[str] = mapped_column(String(250), nullable=False)
+    price: Mapped[str] = mapped_column(String(250), nullable=False)
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
-    comments = relationship("Comment", back_populates="parent_post")
-
-
-class Comment(db.Model):
-    __tablename__ = "comments"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
-    author = relationship("User", back_populates="comments")
-    post_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("blog_posts.id"))
-    parent_post = relationship("BlogPost", back_populates="comments")
-    text: Mapped[str] = mapped_column(String(250), nullable=False)
+    days: Mapped[str] = mapped_column(Integer, nullable=False)
+    destination: Mapped[str] = mapped_column(String(250), nullable=False)
+    popularity: Mapped[str] = mapped_column(Integer, nullable=False)
+    day1: Mapped[str] = mapped_column(String(1000), nullable=False)
+    day2: Mapped[str] = mapped_column(String(1000))
+    day3: Mapped[str] = mapped_column(String(1000))
+    day4: Mapped[str] = mapped_column(String(1000))
+    day5: Mapped[str] = mapped_column(String(1000))
+    day6: Mapped[str] = mapped_column(String(1000))
+    day7: Mapped[str] = mapped_column(String(1000))
+    day8: Mapped[str] = mapped_column(String(1000))
 
 
 with app.app_context():
     db.create_all()
 
 
-# TODO: Use Werkzeug to hash the user's password when creating a new user.
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        email_entered = form.email.data
-        user = db.session.execute(db.select(User).where(User.email == email_entered)).scalar()
-        if user:
-            flash("This email is already registered. Log In!")
-            return redirect("login")
-        else:
-            salted_and_hashed_pwd = generate_password_hash(password=form.password.data,
-                                                           method='scrypt',
-                                                           salt_length=12)
-            new_user = User(email=email_entered,
-                            password=salted_and_hashed_pwd,
-                            name=form.name.data)
-            db.session.add(new_user)
-            db.session.commit()
-
-            login_user(new_user)
-
-            return redirect(url_for('get_all_posts'))
-    return render_template("register.html", form=form, is_logged=current_user.is_authenticated)
-
-
-# TODO: Retrieve a user from the database based on their email. 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        email_entered = form.email.data
-        password = form.password.data
-        user = db.session.execute(db.select(User).where(User.email == email_entered)).scalar()
-        if not user:
-            flash("No account found on this email. Sign up!")
-            return redirect(url_for("login"))
-        elif not check_password_hash(user.password, password):
-            flash("Wrong Password. Please try again.")
-            return redirect(url_for("login"))
-        else:
-            login_user(user)
-            return redirect(url_for('get_all_posts'))
-    return render_template("login.html", form=form, is_logged=current_user.is_authenticated)
-
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('get_all_posts'))
+@app.context_processor
+def show_year():
+    return {"year": dt.datetime.now().year}
 
 
 @app.route('/')
-def get_all_posts():
-    result = db.session.execute(db.select(BlogPost))
-    posts = result.scalars().all()
-    return render_template("index.html", all_posts=posts, is_logged=current_user.is_authenticated)
+def home():
+    return render_template("index.html")
 
 
-# TODO: Allow logged-in users to comment on posts
-@app.route("/post/<int:post_id>", methods=['GET', 'POST'])
-def show_post(post_id):
-    comment_form = CommentForm()
-    requested_post = db.get_or_404(BlogPost, post_id)
-    comments = db.session.execute(db.select(Comment).where(Comment.post_id == post_id)).scalars().all()
-    if comment_form.validate_on_submit():
-        if current_user.is_authenticated:
-            new_comment = Comment(author_id=current_user.id,
-                                  post_id=post_id,
-                                  text=comment_form.comment_text.data)
-            db.session.add(new_comment)
-            db.session.commit()
-            return redirect(url_for("show_post", post_id=post_id))
-        else:
-            flash("You need to login or register to comment")
-            return redirect(url_for("login"))
-    return render_template("post.html", post=requested_post, form=comment_form, comments=comments)
+@app.route('/show_tours')
+def show_tours():
+    destination = request.args.get("destination")
+    result = db.session.execute(db.select(Tour).where(Tour.destination == destination))
+    tours = result.scalars().all()
+    return render_template("show_tours.html", tours=tours, destination=destination.title())
 
 
-# TODO: Use a decorator so only an admin user can create a new post
-@app.route("/new-post", methods=["GET", "POST"])
-@admin_only
-def add_new_post():
-    form = CreatePostForm()
-    if form.validate_on_submit():
-        new_post = BlogPost(
-            author_id=current_user.id,
-            title=form.title.data,
-            subtitle=form.subtitle.data,
-            body=form.body.data,
-            img_url=form.img_url.data,
-            date=date.today().strftime("%B %d, %Y")
-        )
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect(url_for("get_all_posts"))
-    return render_template("make-post.html", form=form, is_logged=current_user.is_authenticated)
+@app.route('/tour_details')
+def tour_details():
+    tour_id = request.args.get('tour_id')
+    result = db.get_or_404(Tour, tour_id)
+    print(result.title)
+    return render_template("tour_details.html", tour=result)
 
 
-# TODO: Use a decorator so only an admin user can edit a post
-@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
-@admin_only
-def edit_post(post_id):
-    post = db.get_or_404(BlogPost, post_id)
-    edit_form = CreatePostForm(
-        title=post.title,
-        subtitle=post.subtitle,
-        img_url=post.img_url,
-        body=post.body
-    )
-    if edit_form.validate_on_submit():
-        post.author_id = current_user.id
-        post.title = edit_form.title.data
-        post.subtitle = edit_form.subtitle.data
-        post.img_url = edit_form.img_url.data
-        post.body = edit_form.body.data
-        db.session.commit()
-        return redirect(url_for("show_post", post_id=post.id))
-    return render_template("make-post.html", form=edit_form, is_edit=True, is_logged=current_user.is_authenticated)
-
-
-# TODO: Use a decorator so only an admin user can delete a post
-@app.route("/delete/<int:post_id>")
-@admin_only
-def delete_post(post_id):
-    post_to_delete = db.get_or_404(BlogPost, post_id)
-    db.session.delete(post_to_delete)
-    db.session.commit()
-    return redirect(url_for('get_all_posts'))
-
-
-@app.route("/delete-comment/<int:comment_id>")
-@only_commenter
-def delete_comment(comment_id):
-    comment_to_delete = db.get_or_404(Comment, comment_id)
-    post_id = comment_to_delete.post_id
-    db.session.delete(comment_to_delete)
-    db.session.commit()
-    return redirect(url_for("show_post", post_id=post_id))
+@app.route('/destinations')
+def destinations():
+    return render_template("destinations.html")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html", is_logged=current_user.is_authenticated)
+    return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route('/cancellation_policy')
+def cancellation_policy():
+    return render_template("cancellation_policy.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html", is_logged=current_user.is_authenticated)
+    contact_form = ContactForm()
+    if contact_form.validate_on_submit():
+        name = contact_form.name.data
+        email = contact_form.email_address.data
+        phone = contact_form.phone.data
+        user_message = contact_form.message.data
+        send_mail(name, email, phone, user_message)
+        contact_form = ContactForm(formdata=None)
+        return render_template("contact.html", message=True, form=contact_form)
+    return render_template("contact.html", message=False, form=contact_form)
+
+
+def send_mail(name, email, phone, user_message):
+    email_msg = f"Name: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {user_message}"
+    company_mail = os.environ.get('company_mail')
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=pwd)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=f"{company_mail}",
+                            msg=f"Subject:User Contact Details\n\n{email_msg}".encode('utf-8'))
 
 
 if __name__ == "__main__":
